f47a250f780f814f12af3395b502c821c2c9f1dc
ShimJiHoon <wlgns2223@gmail.com>
2023-11-29 11:26:23 +0900

    chore: next config i18n 설정

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    // i18n: {
-    //     locales: ["en", "ko"],
-    //     defaultLocale: "en",
-    // },
+    i18n: {
+        locales: ["en", "ko"],
+        defaultLocale: "en",
+    },
 };
 
 module.exports = nextConfig;
